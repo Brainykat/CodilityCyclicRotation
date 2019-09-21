4e3e1eca625f19ab1bbbe4171807ae3db837a275
Chris <eng.christhuku@gmail.com>
2019-09-21 12:45:22 +0300

    Add project files.

---
+++ CyclicRotation.Tests/SolutionTests.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace CyclicRotation.Tests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 8, 9, 7, 6 }, 3, new int[] { 9, 7, 6, 3, 8 })]
+        [InlineData(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 3, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
+        [InlineData(new int[] { 0, 0, 0 }, 1, new int[] { 0, 0, 0 })]
+        [InlineData(new int[] { 1, 2, 3, 4 }, 1, new int[] { 4,1, 2, 3 })]
+        [InlineData(new int[] { 1, 2, 3, 4 }, 2, new int[] { 3,4,1, 2 })]
+        [InlineData(new int[] { 1, 2, 3, 4 }, 3, new int[] { 2, 3, 4,1 })]
+        [InlineData(new int[] { 1, 2, 3, 4 }, 4, new int[] { 1, 2, 3, 4 })]
+        public void solution_RotatesArray_N_Times(int [] a, int k, int [] result)
+        {
+            Solution sln = new Solution();
+            var res = sln.solution(a, k);
+            Assert.Equal(result, res);
+        }
+    }
+}
---
+++ CyclicRotation/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CyclicRotation
+{
+    public class Solution
+    {
+        public int[] solution(int[] A, int K)
+        {
+            if (K <= 0)
+                return A;
+            if (A.Length == 0) return A;
+            var linkedList = new LinkedList<int>(A);
+            for (var i = 0; i < K; ++i)
+            {
+                var node = linkedList.Last;
+                linkedList.RemoveLast();
+                linkedList.AddFirst(node);
+            }
+            return linkedList.ToArray();
+        }
+       public int[] CounterClockwise(int[] A, int K)
+        {
+            return A.Skip(K) 
+             .Concat(A.Take(K)) 
+             .ToArray();
+        }
+    }
+}
